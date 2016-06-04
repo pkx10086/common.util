dca654aa2848992632e25c958ccc6528f8c8d845
pkx10086 <1028764968@qq.com>
2016-06-04 14:28:50 +0800

    Utility

--- src/main/java/common/util/PropertiesUtil.java
+++ src/main/java/common/util/PropertiesUtil.java
@@ -8,24 +8,44 @@ import java.io.InputStream;
 import java.util.Properties;
 
 /**
-* ��˵������ȡ��׺Ϊ.properties�ļ��Ĺ�����
-* @author pankx
-* @date 2016��5��25�� ����2:51:05
-*/
+ * 功能描述：Properties 工具类
+ * @author pankx
+ * @date 2016年5月30日
+ */
 public class PropertiesUtil {
-	private static InputStream inputStream  =null;
 	
-	static{
-		try {
-			inputStream = new FileInputStream(new File("src/main/resources/config.properties"));
-		} catch (FileNotFoundException e) {
-			 e.printStackTrace();
-		}
+	private static  PropertiesUtil propertiesUtil;
+	private static InputStream inputStream  =null;
+	 
+	private PropertiesUtil(){
 	
 	}
 	
-	public static String readValueBykey(String key){
-		 Properties props=new Properties();
+	public static synchronized   PropertiesUtil getProperties(){
+		try {
+	    	  //inputStream=Thread.currentThread().getContextClassLoader().getResourceAsStream("");
+	    	  //inputStream = new FileInputStream(new File("src/main/resources/config.properties"));
+	    	  //需要测试动态获取配置配件信息
+			   inputStream = new FileInputStream(new File(Thread.currentThread().getContextClassLoader().getResource("").getFile()+"config.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		 
+		if(propertiesUtil==null){
+			  synchronized (PropertiesUtil.class) {  
+			        if (propertiesUtil == null) {  
+			        	propertiesUtil = new PropertiesUtil();  
+			        }  
+		   } 
+		}
+		
+		return propertiesUtil;
+	}
+	
+	
+	public  String readValueBykey(String key){
+	  
+		Properties props=new Properties();
 		try {
 			props.load(inputStream);
 			return props.getProperty(key);
@@ -36,10 +56,16 @@ public class PropertiesUtil {
 		return null;
 	}
 	
+	
+	
+	
 	public static void main(String args[]){
 		
+	
+//		System.out.println(Thread.currentThread().getContextClassLoader().getResource("").getFile()+"config.properties");
+	//	System.out.println(ClassLoader.getSystemResource(""));
 		//System.out.println(System.getProperty("user.dir"));
 		//System.out.println(PropertiesUtil.class.getClassLoader().getResourceAsStream("main/resources/config.properties"));//.getgetResourceAsStream("src/main/resources/config.properties"));
-		System.out.println("[value]"+PropertiesUtil.readValueBykey("quarzt.schedule.time"));
+	  // System.out.println("[value]"+PropertiesUtil.readValueBykey("quarzt.schedule.time"));
 	}
 }
